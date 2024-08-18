925f0310d2cfcb9cc0e2c8ab240e24f5edf8d025
muhammad ahmed noorani <ahmednoorani258@gmail.com>
2024-08-18 06:00:22 -0700

    changes update

--- src/app/components/navigationbar.tsx
+++ src/app/components/navigationbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar (){
     
     return(
         <nav className=" text-white flex justify-evenly w-[45%] lg:w-[40%]  ">
-            <a href="/"  className="ease-in-out duration-500 text-glow">Home</a>
+            <a href="/"  className="hover:text-yellow-500 ease-in-out duration-500 text-glow">Home</a>
             <a href="about" className=" hover:text-yellow-500 ease-in-out duration-500 text-glow">About</a>
             <a href="contact" className=" hover:text-yellow-500 ease-in-out duration-500 text-glow">Contact</a>
         </nav>
